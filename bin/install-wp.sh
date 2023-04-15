44ea60ee0884f0781581093815385a77ecacb077
Victorio Bolzani <vbolzani@flowgate.net>
2023-04-15 15:53:37 -0300

    a

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -6,7 +6,7 @@ wp core install \
   --admin_user="admin" \
   --admin_password="admin" \
   --admin_email="admin@example.com" \
-  --url="http://vulnerablewordpressfg.herokuapp.com/:8081/" \
+  --url="http://vulnerablewordpressfg.herokuapp.com:80/" \
   --skip-email
 
 # Install plugin remotely 
